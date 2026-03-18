b5a24ad36e7cc373baadaa9d250b5b2f0b4aeb2a
girimNam <rlfla@naver.com>
2026-03-18 14:05:27 +0900

    fix: App.jsx 수정

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,23 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Header from './components/header'
 import Input from './components/input'
+import List from './components/List'
 
 function App() {
   const [todos, setTodos] = useState([])
 
+  /* 투두리스트 저장 및 불러오기 설정, useEffect 이용 */
+  useEffect(() => {
+    const saved = JSON.parse(localStorage.getItem('myTodoList')) || []
+    setTodos(saved)
+  }, [])
 
+  useEffect(() => {
+    localStorage.setItem('myTodoList', JSON.stringify(todos))
+  }, [todos])
 
   return (
-    <>
-    <div className='min-h-screen flex justify-center'>
-      <Header />
-      <Input todos={todos} setTodos={setTodos} />
-      <List todos={todos} setTodos={setTodos} />
-
+    <div id="app">
+      <div className="min-h-screen flex justify-center">
+        <Header />
+        <Input todos={todos} setTodos={setTodos} />
+        <List todos={todos} setTodos={setTodos} />
+      </div>
     </div>
-      
-    </>
   )
 }
 
