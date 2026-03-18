00be907332ff34311b3823cdd4217cc1899644bf
girimNam <rlfla@naver.com>
2026-03-18 13:58:44 +0900

    feat: Input 할일 입력창 추가

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/header'
+import Input from './components/input'
 
 function App() {
+  const [todos, setTodos] = useState([])
+
 
 
   return (
     <>
     <div className='min-h-screen flex justify-center'>
       <Header />
+      <Input todos={todos} setTodos={setTodos} />
+      <List todos={todos} setTodos={setTodos} />
+
     </div>
       
     </>
--- react-todo/src/components/Input.jsx
+++ react-todo/src/components/Input.jsx
@@ -1,4 +1,32 @@
-function Input(){
+import { useState } from 'react'
 
+function Input({ todos, setTodos }) {
+  const [input, setInput] = useState('')
+
+  const clickSubmit = (e) => {
+    e.preventDefault()
+
+    if (input.trim() === '') {
+      alert('내용을 입력해주세요.')
+      return
+    }
+
+    setTodos([...todos, { text: input, done: false }])
+    setInput('')
+  }
+
+  return (
+    <section className="inputlist">
+      <form onSubmit={clickSubmit}>
+        <input value={input} onChange={onChange} placeholder="TO DO 추가" />
+        <button type="submit">추가</button>
+      </form>
+
+      <div>
+        남은 TO DO 개수:{' '}
+        <span>{todos.filter((todo) => !todo.done).length}</span>개
+      </div>
+    </section>
+  )
 }
 export default Input
