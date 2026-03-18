0a22d66e36d540d759f77bbde58b1590d99e6517
girimNam <rlfla@naver.com>
2026-03-18 14:55:59 +0900

    fix: 함수 주석 수정

--- react-todo/src/components/List.jsx
+++ react-todo/src/components/List.jsx
@@ -1,11 +1,11 @@
 function List({ todos, setTodos }) {
   const deleteButton = (index) => {
-    setTodos(todos.filter((_, i) => i !== index)) //삭제할 인덱스 i만 빼고 남겨라
+    setTodos(todos.filter((_, i) => i !== index)) //삭제할 인덱스 i만 빼고 남겨라, _는 배열 안의 값(안씀)
   }
 
   const checkBox = (index) => {
-    const newTodos = [...todos]
-    newTodos[index].done = !newTodos[index].done
+    const newTodos = [...todos] //기존 배열을 직접 수정하는게 아니라 복사 후 수정
+    newTodos[index].done = !newTodos[index].done //done은 todo에 들어가는 객체의 속성
     setTodos(newTodos)
   }
 
